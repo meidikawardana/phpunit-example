ca7f1028b9a684f177703045e09a8f2369ee9d78
meidikawardana <jdeep555.mw@gmail.com>
2020-10-13 16:48:31 +0700

    intendedly make error so build will fail / error

--- src/SimpleCalc.php
+++ src/SimpleCalc.php
@@ -4,9 +4,9 @@ class SimpleCalc
 {
 	public function addPercentage($value, $percentage)
 	{
-		if (!is_numeric($percentage) || $percentage < 0) {
-			 throw new \Exception('Invalid Percentage');
-		}		
+//		if (!is_numeric($percentage) || $percentage < 0) {
+//			 throw new \Exception('Invalid Percentage');
+//		}
 		
 		$result = $value + (abs($value) * ($percentage / 100));
 		if ((int)$result == $result) {
